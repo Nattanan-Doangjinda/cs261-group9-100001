01ef922c3f742d4291ed759b81c9ae9f6636eda8
Rattasat Onnomdee - 6609612186 <144299401+RattasatOnnomdee@users.noreply.github.com>
2024-11-11 21:26:50 +0700

    Push createFrom into main branch

---
+++ public/createForm.js
@@ -0,0 +1,24 @@
+const addSubject = document.getElementById("addSubject")
+const withDraw = document.getElementById("withDraw")
+const resign = document.getElementById("resign")
+const crossProgram = document.getElementById("crossProgram")
+const userId = localStorage.getItem('userId');
+let requestFormId = localStorage.getItem("requestFormId");
+
+addSubject.addEventListener("click" , () => {
+    window.location.href = "request_add_subject.html";
+    // localStorage.setItem("editingExistingDraft", "true");
+})
+
+withDraw.addEventListener("click",() => {
+    window.location.href = "request_withdraw.html";
+})
+
+resign.addEventListener("click",() => {
+    window.location.href = "resignation_form.html";
+})
+
+crossProgram.addEventListener("click",() => {
+    window.location.href = "request_cross_program.html";
+})
+
