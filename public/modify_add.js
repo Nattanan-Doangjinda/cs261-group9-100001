4299058bff03b4ec34bc4cbf32d52436a477ef58
Meen832548 <k0925357882@gmail.com>
2024-11-11 19:33:23 +0700

    feat: combine file to main

---
+++ public/modify_add.js
@@ -0,0 +1,260 @@
+window.onload = async function() {
+  const urlParams = new URLSearchParams(window.location.search);
+  const requestFormId = urlParams.get('requestFormId');
+    const header = {
+        "Content-Type": "application/json"
+    };
+    const url = "http://localhost:8000/user/request/"+requestFormId;
+    const response = await fetch(url, {
+        method: "GET",
+        headers: header,
+    });
+    if (response.ok) {
+        const data = await response.json();
+        //ชื่อ
+        const studentName = data.details.studentName;
+        const nameParts = studentName.split(" ");
+        const title = nameParts[0] || '';  
+        const firstName = nameParts[1] || '';  
+        const lastName = nameParts[2] || '';  
+        
+        //address
+        const address =data.details.address;
+        const addressParts = address.split(" ");
+        const no = addressParts[0] || ''; 
+        const sub_district = addressParts[1] || '';
+        const district = addressParts[2] || '';
+        const province = addressParts[3] || '';
+        
+        //term year
+        const termYear = data.details.Semester;
+        const term = termYear.split('/')[0];
+        const year = termYear.split('/')[1];
+        
+        document.getElementById('title').value = title; 
+        document.getElementById('first_name').value = firstName;
+        document.getElementById('last_name').value = lastName;
+        document.getElementById('student_id').value = data.details.studentId;
+        document.getElementById('semester').value = data.details.year;
+        document.getElementById('address').value = no;
+        document.getElementById('sub_district').value = sub_district;
+        document.getElementById('district').value = district;
+        document.getElementById('province').value = province;
+        document.getElementById('phone').value = data.details.contact;
+        document.getElementById('guardian_phone').value = data.details.parentContactNumber;
+        document.getElementById('advisor').value = data.details.advisor;
+        document.getElementById('term').value = term;
+        document.getElementById('year').value = year;
+        document.getElementById('course_code').value = data.details.courseId;
+        document.getElementById('course_name').value = data.details.courseName;
+        document.getElementById('section').value = data.details.section;
+        document.getElementById('reason').value = data.details.requestReason;
+
+    } else {
+        console.log("Error:", response.status);
+        alert("ไม่สามารถดึงข้อมูลได้");
+    }
+}
+
+document.addEventListener("DOMContentLoaded", async function(event)  {
+  const urlParams = new URLSearchParams(window.location.search);
+  const userID = urlParams.get('userID');
+  const requestFormId = urlParams.get('requestFormId');
+
+    const submitButton = document.querySelector('.submit');
+    submitButton.addEventListener('click', function(event) {
+      if (address.value === "") {
+        address.value = "-";
+      }
+      if (sub_district.value === "") {
+        sub_district.value = "-";
+      }
+      if (district.value === "") {
+        district.value = "-";
+      }
+      if (province.value === "") {
+        province.value = "-";
+      }
+        event.preventDefault();
+        const form = document.querySelector('form');
+          if (form.checkValidity()) {
+            savedFormData(form);
+            ReturnToHomepage()
+          } else {
+              alert('กรุณากรอกข้อมูลให้ครบทุกช่อง!');
+          }
+    });
+  
+    const saveDraftButton = document.querySelector('.save-draft');
+    saveDraftButton.addEventListener('click', function(event) {
+        event.preventDefault();
+        saveDraft();
+        ReturnToHomepage()
+    });
+  
+    const cancelButton = document.querySelector('.cancel');
+    cancelButton.addEventListener('click', function(event) {
+      ReturnToHomepage()
+    });
+  
+    async function savedFormData() {
+      const savedData = new FormData(document.querySelector('form'));
+      const formDataObj = formDataToJSON(savedData); 
+      const Datefrom = getCurrentDateTime();
+      console.log('ข้อมูลที่ถูกบันทึก:', formDataObj);
+      const header = {
+          "Content-Type": "application/json"
+      };
+  
+      const body = JSON.stringify({
+          "status": "รอดำเนินการ",
+          "state": "Published",
+          "type": "ขอจดทะเบยีนเพิ่มวิชา",
+          "details": {
+              "date" : Datefrom,
+              "studentName": formDataObj.title+" "+formDataObj.first_name+" "+formDataObj.last_name,
+              "studentId": formDataObj.student_id,
+              "year": formDataObj.semester,
+              "address": formDataObj.address + " " + formDataObj.sub_district + " " + formDataObj.district + " " + formDataObj.province,
+              "contact": formDataObj.phone,
+              "parentContactNumber": formDataObj.guardian_phone,
+              "advisor": formDataObj.advisor,
+              "sinceSemester": "",
+              "Semester": formDataObj.term + "/" + formDataObj.year,
+              "courseId": formDataObj.course_code,
+              "courseName": formDataObj.course_name,
+              "section": formDataObj.section,
+              "requestReason": formDataObj.reason
+          }
+      });
+  
+      const url = "http://localhost:8000/user/"+userID;
+      const response = await fetch(url, {
+          method: "POST",
+          headers: header,
+          body: body,
+      });
+  
+      if (response.ok) {
+        showModal(); // แสดง pop-up เมื่อส่งฟอร์มสำเร็จ
+      } else {
+        alert('เกิดข้อผิดพลาดในการส่งข้อมูล');
+      }
+      Delete(requestFormId);
+  }
+  
+  
+    async function saveDraft() {
+      const savedData = new FormData(document.querySelector('form'));
+      const formDataObj = formDataToJSON(savedData); 
+      const Datefrom = getCurrentDateTime();
+      console.log('ข้อมูลที่ถูกบันทึก:', formDataObj);
+      const header = {
+          "Content-Type": "application/json"
+      };
+  
+      const body = JSON.stringify({
+          "details": {
+              "date" : Datefrom,
+              "studentName": formDataObj.title+" "+formDataObj.first_name+" "+formDataObj.last_name,
+              "studentId": formDataObj.student_id,
+              "year": formDataObj.semester,
+              "address": formDataObj.address + " " + formDataObj.district + " " + formDataObj.sub_district + " " + formDataObj.province,
+              "contact": formDataObj.phone,
+              "parentContactNumber": formDataObj.guardian_phone,
+              "advisor": formDataObj.advisor,
+              "sinceSemester": "",
+              "Semester": formDataObj.term + "/" + formDataObj.year,
+              "courseId": formDataObj.course_code,
+              "courseName": formDataObj.course_name,
+              "section": formDataObj.section,
+              "requestReason": formDataObj.reason
+          }
+      });
+  
+      const url = "http://localhost:8000/user/"+requestFormId;
+      const response = await fetch(url, {
+          method: "PUT",
+          headers: header,
+          body: body,
+      });
+  
+      if (response.ok) {
+        showModal2(); // แสดง pop-up เมื่อส่งฟอร์มสำเร็จ
+      } else {
+        alert('เกิดข้อผิดพลาดในการส่งข้อมูล');
+      }
+    }
+  
+    function ReturnToHomepage(){
+      window.location.href = '../views/homepage.html'; 
+    }
+  
+    function formDataToJSON(formData) {
+      const obj = {};
+      formData.forEach((value, key) => {
+        obj[key] = value;
+      });
+      return obj;
+    }
+    
+    function getCurrentDateTime() {
+      const currentDate = new Date();
+      const day = currentDate.getDate();
+      const month = currentDate.getMonth() + 1; // เดือนเริ่มจาก 0
+      const year = currentDate.getFullYear();
+      const hours = String(currentDate.getHours()).padStart(2, '0');
+      const minutes = String(currentDate.getMinutes()).padStart(2, '0');
+      return `${day}/${month}/${year} ${hours}:${minutes}`;
+    }
+    
+    const showModal = () => {
+      document.getElementById('successModal').style.display = 'block';
+    };
+  
+    const closeModal = () => {
+      document.getElementById('successModal').style.display = 'none';
+    };
+  
+    const closeModalButton = document.getElementById('closeModal'); 
+    if (closeModalButton) {
+      closeModalButton.addEventListener('click', function() {
+        closeModal(); // เมื่อกดปุ่ม "ตกลง" ให้ปิด modal
+      });
+    }
+
+    
+  const showModal2 = () => {
+    document.getElementById('successModal2').style.display = 'block';
+  };
+
+  const closeModal2 = () => {
+    document.getElementById('successModal2').style.display = 'none';
+  };
+
+  const closeModalButton2 = document.getElementById('closeModal2'); 
+  if (closeModalButton2) {
+    closeModalButton2.addEventListener('click', function() {
+      closeModal2(); // เมื่อกดปุ่ม "ตกลง" ให้ปิด modal
+    });
+  }
+  async function Delete(re){
+    const header = {
+      "Content-Type": "application/json"
+    };
+    const body = JSON.stringify({
+      
+    });
+    const url2 = "http://localhost:8000/user/"+re;
+    await fetch(url2, {
+      method: "DELETE",
+      headers: header,
+      body: body,
+    });
+    if (response.ok) {
+      
+    } else {
+      
+    }
+  }
+  });
